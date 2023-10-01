a8904347b11769ec6f1aa045e371aee5cfa83be1
donBarbos <donbarbos@proton.me>
2023-10-01 13:23:17 +0300

    database don't change, fix default vars

--- bot/config.py
+++ bot/config.py
@@ -6,16 +6,14 @@ import random
 import time
 
 load_dotenv()
-CELI_SITE_URL: str = (
-    os.getenv("CELI_SITE_URL")
-    if os.getenv("CELI_SITE_URL")
-    else "https://som.yale.edu/story/2022/over-1000-companies-have-curtailed-operations-russia-some-remain"
+CELI_SITE_URL: str  = (
+    os.getenv('CELI_SITE_URL')
+    or "https://som.yale.edu/story/2022/over-1000-companies-have-curtailed-operations-russia-some-remain"
 )
-BROWSER_DIR: str = os.getenv("BROWSER_DIR") if os.getenv("BROWSER_DIR") else "my_browser"
+BROWSER_DIR: str = os.getenv('BROWSER_DIR') or "my_browser"
 PATH_TO_DUMP: str = (
-    os.getenv("PATH_TO_DUMP")
-    if os.getenv("PATH_TO_DUMP")
-    else "Yale_CELI_List_of_Companies_Leaving_and_Staying_in_Russia.csv"
+    os.getenv('PATH_TO_DUMP')
+    or "Yale_CELI_List_of_Companies_Leaving_and_Staying_in_Russia.csv"
 )
 
 # for more realism: sleep(1) = time.sleep(1+-0.2), with slippage 0.2s
